afef0f2087ff885800e944fda67b7d602e99939c
isu <natantrojak@wp.pl>
2026-03-03 00:18:00 +0100

    Added printing bongo cat on right oled screen

--- sofle21/keymap.c
+++ sofle21/keymap.c
@@ -1,8 +1,6 @@
 #include "config.h"
 #include QMK_KEYBOARD_H
-// #if __has_include("keymap.h")
-// #    include "keymap.h"
-// #endif
+#include <stdio.h>
 
 #define INDICATOR_BRIGHTNESS 30
 
@@ -233,18 +231,25 @@ void keyboard_post_init_user(void) {
 }
 #endif
 
-// #define OLED_ENABLE
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
+    [_LOWER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
+    [_RAISE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
+    #ifdef DYNAMIC_TAPPING_TERM_ENABLE
+    [_ADJUST] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(DT_DOWN, DT_UP), },
+    #else
+    [_ADJUST] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
+    #endif
+    [_GAMING] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
+    [_GAMING_R2L] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
+};
+#endif
+
 #ifdef OLED_ENABLE
 
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
-}
+#include "bongo.h"
+char wpm_str[10];
 
 static void print_status_narrow(void) {
     oled_write_ln_P(PSTR("NTr"), false);
@@ -282,8 +287,8 @@ static void print_status_narrow(void) {
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;
     }
     return rotation;
 }
@@ -292,40 +297,9 @@ bool oled_task_user(void) {
     if (is_keyboard_master()) {
         print_status_narrow();
     } else {
-        render_logo();
+        draw_bongo();
     }
     return false;
 }
 
 #endif
-
-// #define ENCODER_MAP_ENABLE
-#ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [_LOWER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [_RAISE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    #ifdef DYNAMIC_TAPPING_TERM_ENABLE
-    [_ADJUST] = { ENCODER_CCW_CW(DT_DOWN, DT_UP), },
-    #else
-    [_ADJUST] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    #endif
-    [_GAMING] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [_GAMING_R2L] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-};
-
-// const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-//     [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
-//     [_LOWER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
-//     [_RAISE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
-//     #ifdef DYNAMIC_TAPPING_TERM_ENABLE
-//     [_ADJUST] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(DT_DOWN, DT_UP), },
-//     #else
-//     [_ADJUST] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
-//     #endif
-//     [_GAMING] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
-//     [_GAMING_R2L] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), },
-// };
-#endif
-
-
